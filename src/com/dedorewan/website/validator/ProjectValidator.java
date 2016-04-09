22b016921cb4bc4b67409abd514075a07fae38d6
51103 <51103@DESKTOP-T0FS3NQ>
2016-04-09 12:47:53 +0700

    Update Validator

--- src/com/dedorewan/website/validator/ProjectValidator.java
+++ src/com/dedorewan/website/validator/ProjectValidator.java
@@ -1,11 +1,14 @@
 package com.dedorewan.website.validator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -30,66 +33,49 @@ public class ProjectValidator implements Validator {
 		Locale locale = LocaleContextHolder.getLocale();
 		String message = "";
 		Project project = (Project) target;
-		StringBuilder visas = new StringBuilder("");
-		boolean first = true;
+		List<String> visas = new ArrayList<String>();
 		for (String visa : project.getMembers()) {
 			if (!projectService.visaExsisted(visa)) {
-				if (first) {
-					visas.append(visa);
-					first = false;
-				} else {
-					visas.append("," + visa);
-				}
+				visas.add(visa);
 			}
 		}
+		String visaString = StringUtils.collectionToCommaDelimitedString(visas);
 		if (project.getProjectNumber() == null) {
-			errors.rejectValue("projectNumber", "NotNull",
-					"Project Number must not be left empty");
+			errors.rejectValue("projectNumber", "NotNull", "Project Number must not be left empty");
 		} else if (project.getProjectNumber() <= 0) {
-			errors.rejectValue("projectNumber", "NotLessThan",
-					"Project Number must be greater than 0");
+			errors.rejectValue("projectNumber", "NotLessThan", "Project Number must be greater than 0");
 		} else if (project.getProjectNumber() >= 10000) {
-			errors.rejectValue("projectNumber", "GreaterThan",
-					"Project Number must be 4 digits");
+			errors.rejectValue("projectNumber", "GreaterThan", "Project Number must be 4 digits");
 		}
 		if (project.getName().length() == 0) {
-			errors.rejectValue("name", "NotEmpty",
-					"Project Name must not be left empty");
+			errors.rejectValue("name", "NotEmpty", "Project Name must not be left empty");
 		}
 		if (project.getName().length() > 50) {
-			errors.rejectValue("name", "InvalidLength",
-					"Project Name must be less than 50 characters");
+			errors.rejectValue("name", "InvalidLength", "Project Name must be less than 50 characters");
 		}
 		if (project.getCustomer().length() > 50) {
-			errors.rejectValue("customer", "InvalidLength",
-					"Customer Name must be less than 50 characters");
+			errors.rejectValue("customer", "InvalidLength", "Customer Name must be less than 50 characters");
 		}
 		if (project.getCustomer().length() == 0) {
-			errors.rejectValue("customer", "NotEmpty",
-					"Customer Name must not be left empty");
+			errors.rejectValue("customer", "NotEmpty", "Customer Name must not be left empty");
 		}
 		if (project.getGroupId() == null) {
 			errors.rejectValue("groupId", "NotNull", "Must choose a group");
 		}
 		if (project.getStartDate() == null) {
-			errors.rejectValue("startDate", "NotNull",
-					"Must choose a startingDate");
+			errors.rejectValue("startDate", "NotNull", "Must choose a startingDate");
 		}
 		if (project.getEndDate() != null && project.getStartDate() != null
 				&& project.getEndDate().before(project.getStartDate())) {
-			errors.rejectValue("endDate", "DateNotValid",
-					"End Date must be after Start Date");
+			errors.rejectValue("endDate", "DateNotValid", "End Date must be after Start Date");
 		}
-		if (projectService.projectNumberExisted(project.getId(),project.getProjectNumber())) {
-			message = messageSource.getMessage("errors.projectNumberExisted",
-					new Object[] {}, locale);
+		if (projectService.projectNumberExisted(project.getId(), project.getProjectNumber())) {
+			message = messageSource.getMessage("errors.projectNumberExisted", new Object[] {}, locale);
 			errors.rejectValue("projectNumber", "projectNumberExisted", message);
 		}
-		if (!visas.toString().equals("")) {
-			message = messageSource.getMessage("errors.InvalidVisa",
-					new Object[] {}, locale);
-			errors.rejectValue("members", "InvalidVisa",
-					message + visas.toString() + "}");
+		if (visaString != null && !visaString.equals("")) {
+			message = messageSource.getMessage("errors.InvalidVisa", new Object[] {}, locale);
+			errors.rejectValue("members", "InvalidVisa", message + visaString + "}");
 		}
 
 	}
